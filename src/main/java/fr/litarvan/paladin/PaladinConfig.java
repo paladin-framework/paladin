380340cc5706b47e35eafd592dc3cbe1c8a873d2
Adrien Navratil <adrien1975@live.fr>
2018-05-22 12:42:24 +0200

    Working on simple config system

--- src/main/java/fr/litarvan/paladin/PaladinConfig.java
+++ src/main/java/fr/litarvan/paladin/PaladinConfig.java
@@ -1,13 +1,66 @@
 package fr.litarvan.paladin;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 public class PaladinConfig
 {
-    public final int port;
-    public final boolean disableLogs;
+    private static final ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    private Map<String, ? super Object> content;
 
-    public PaladinConfig(int port, boolean disableLogs)
+    public PaladinConfig(Map<String, ? super Object> content)
     {
-        this.port = port;
-        this.disableLogs = disableLogs;
+        this.content = content;
+    }
+
+    public <T> T get(String key, Class<T> type)
+    {
+        return get(key);
+    }
+
+    public <T> T get(String key)
+    {
+        return (T) content.get(key);
+    }
+
+    void set(String key, Object value)
+    {
+        content.put(key, value);
+    }
+
+    public static PaladinConfig load(String path) throws IOException
+    {
+        if (path == null)
+        {
+            path = "./";
+        }
+
+        if (path.endsWith("/"))
+        {
+            path += "config.json";
+        }
+
+        File file = new File(path);
+        file.getParentFile().mkdirs();
+
+        if (!file.canRead())
+        {
+            System.err.println("Can't read config file '" + file.getAbsolutePath() + "'");
+            System.exit(1);
+        }
+
+        if (!file.exists())
+        {
+            System.err.println("Config file '" + file.getAbsolutePath() + "' doesn't exist but can't write default config in it");
+            System.exit(1);
+        }
+
+        return new PaladinConfig(mapper.readValue(file, new TypeReference<HashMap<String, ?>>() {}));
     }
 }
