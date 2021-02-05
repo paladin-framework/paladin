16444b5957630cbbf10d51da7494e1fbb3ebce25
Adrien Navratil <adrien1975@live.fr>
2021-02-05 11:22:52 +0100

    Version bump

--- src/main/java/fr/litarvan/paladin/Paladin.java
+++ src/main/java/fr/litarvan/paladin/Paladin.java
@@ -36,7 +36,7 @@ public class Paladin
 {
     private static final Logger log = LoggerFactory.getLogger("Paladin");
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.2.3";
 
     private Object app;
     private PaladinApp appInfo;
